6b46c3b40aa4f485cc5104df6dc5c685c8a35a3c
Orville Huang <ooorville@gmail.com>
2020-11-18 19:05:42 -0500

    Update NewSale

--- PRIMEWeb/Sales/NewSale.aspx.cs
+++ PRIMEWeb/Sales/NewSale.aspx.cs
@@ -11,26 +11,32 @@ namespace PRIMEWeb.Sales
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
-        }
-
-        protected void btnAddOrder_Click(object sender, EventArgs e)
-        {
-            pnlBtnSales.Visible = false;
-            pnlOrder.Visible = true;
-        }
-
-        protected void btnSaveOrder_Click(object sender, EventArgs e)
-        {
-            pnlBtnSales.Visible = true;
-            pnlOrder.Visible = false;
-            ddlProduct.SelectedIndex = 0;
-            txtQty.Text = txtNote.Text = String.Empty;
+            if (IsPostBack) return;
+            txtDate.Text = DateTime.Today.ToShortDateString();
         }
 
         protected void ddlProduct_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnSaveOrder_Click(object sender, EventArgs e)
+        {
+            lsbOrders.Items.Add(new ListItem(ddlProduct.SelectedItem.Text, txtQty.Text));
+            ddlProduct.SelectedIndex = 0;
+            txtQty.Text = txtNote.Text = String.Empty;
+            txtPrice.Text = "Price";
+            txtStock.Text = "Number in Stock...";
+        }
+
+        protected void btnDeleteOrder_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void lsbOrders_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtQty.Text = lsbOrders.SelectedValue;
         }
     }
 }
