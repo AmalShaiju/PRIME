7f9139e89144cbbc35b71347e0471ed41bdd8427
Orville Huang <ooorville@gmail.com>
2020-12-18 11:13:00 -0500

    fix Edit

--- PRIMEWeb/Inventory/EditItem.aspx.cs
+++ PRIMEWeb/Inventory/EditItem.aspx.cs
@@ -12,101 +12,93 @@ namespace PRIMEWeb.Inventory
 {
     public partial class EditItem : System.Web.UI.Page
     {
-        private static int id = -1;
-        static InventoryDataSet dsInventory = new InventoryDataSet();
-        private static DataRow[] rows;
-        static EditItem()
-        {
-            dsInventory = new InventoryDataSet();
-
-            try
-            {
-                InventoryTableAdapter daInventory = new InventoryTableAdapter();
-                daInventory.Fill(dsInventory.Inventory);
-            }
-            catch { }
-        }
+        private static InventoryDataSet dsInventory = new InventoryDataSet();
+        private static InventoryTableAdapter daInventory = new InventoryTableAdapter();
+        private static InventoryLookUpTableAdapter daInvLookUp = new InventoryLookUpTableAdapter();
+        private static ProductTableAdapter daProduct = new ProductTableAdapter();
+        private int id;
 
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!User.Identity.IsAuthenticated)  //if not logged in
                 Response.Redirect("/");
 
-            InventoryLookUpTableAdapter dainventoryLookUpTable = new InventoryLookUpTableAdapter();
-            dainventoryLookUpTable.Fill(dsInventory.InventoryLookUp);
-            id = Convert.ToInt32(Session["InventoryId"]);
-            Label1.Text = Session["InventoryId"].ToString();
-            if(! IsPostBack)
+            if (String.IsNullOrEmpty(Request.QueryString["ID"]))
+                Response.Redirect("/Inventory");
 
-                if (id != -1)
-                {
-                    try
-                    {
-                        DataRow ServiceOrder = dsInventory.InventoryLookUp.FindByid(id); // Find the related Record and fill the fields in the page with the data
+            try
+            {
+                dsInventory.Clear();
+                daInventory.Fill(dsInventory.Inventory);
+                daInvLookUp.Fill(dsInventory.InventoryLookUp);
+                daProduct.Fill(dsInventory.Product);
+            }
+            catch { }
 
-                   
-                    
-                            this.txtDescription.Text = ServiceOrder.ItemArray[6].ToString();
-                            this.txtQuantity.Text = ServiceOrder.ItemArray[1].ToString();
-                            this.txtPrice.Text = ServiceOrder.ItemArray[3].ToString();
-                            this.txtBrand.Text = ServiceOrder.ItemArray[5].ToString();
+            if (IsPostBack) return;
 
-                            this.txtSize.Text = ServiceOrder.ItemArray[2].ToString();
+            try
+            {
+                id = Convert.ToInt32(Request.QueryString["ID"]);
+                DataRow ServiceOrder = dsInventory.InventoryLookUp.FindByid(id); // Find the related Record and fill the fields in the page with the data
 
-                            this.ddlMeasures.SelectedValue = ServiceOrder.ItemArray[8].ToString();
-                            this.ddlProducts.SelectedValue = ServiceOrder.ItemArray[0].ToString();
-                       
-                    
+                txtDescription.Text = ServiceOrder.ItemArray[6].ToString();
+                txtQuantity.Text = ServiceOrder.ItemArray[1].ToString();
+                txtPrice.Text = ServiceOrder.ItemArray[3].ToString();
+                txtBrand.Text = ServiceOrder.ItemArray[5].ToString();
 
-                   }
-                   catch { }
-                    }
+                txtSize.Text = ServiceOrder.ItemArray[2].ToString();
+
+                ddlMeasures.SelectedValue = ServiceOrder.ItemArray[8].ToString();
+                ddlProducts.SelectedValue = ServiceOrder.ItemArray[0].ToString();
+            }
+            catch (Exception)
+            {
+                lblMessage.Text = "Unable to load data";
+                lblMessage.Visible = true;
+            }
         }
 
-        protected void btnAddItem_Click(object sender, EventArgs e)
+        protected void ddlProducts_SelectedIndexChanged(object sender, EventArgs e)
         {
-           
-            
-                if (id != -1)
-                {
-                   // try
-                    //{
-                        DataRow record = dsInventory.Inventory.FindByid(id); // Find the related Record and fill the fields in the page with the data
+            DataRow prod = dsInventory.Product.FindByid(Convert.ToInt32(ddlProducts.SelectedValue));
+            txtDescription.Text = prod[2].ToString();
+            txtBrand.Text = prod[3].ToString();
+        }
 
-                        if (record != null)
-                        {
-                        //record[5] = this.ddlProducts.SelectedValue;
-                        record[3] = this.ddlMeasures.SelectedValue;
-                        record[1] = this.txtQuantity.Text;
-                        record[2] = this.txtSize.Text;
-                        record[4] = this.txtPrice.Text;
-
-                      // dsInventory.inventory.Rows.Add(record); // add the rows to the dataset
-
-                            InventoryTableAdapter daInventory = new InventoryTableAdapter();
-                            daInventory.Update(record);
-                            // Call update method on the service adapter so it updates the table in memory ( All changes made are applied - CRUD)
-                            dsInventory.AcceptChanges();
-
-                        Response.Redirect("Default.aspx");
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            id = Convert.ToInt32(Request.QueryString["ID"]);
+            DataRow record = dsInventory.Inventory.FindByid(id);
+            // Find the related Record and fill the fields in the page with the data
 
 
-                        }
+            try
+            {
+                record[1] = txtQuantity.Text;
+                record[2] = txtSize.Text;
+                record[5] = ddlProducts.SelectedValue;
+                record[3] = ddlMeasures.SelectedValue;
+                record[4] = txtPrice.Text;
 
-                    //}
-                    //catch
-                    //{
-                        // this.Label1.Visible = true;
-                        // this.Label1.Text = "Database Error";
-
-                    //}
-                }
-            
+                daInventory.Update(record);
+                // Call update method on the service adapter so it updates the table in memory ( All changes made are applied - CRUD)
+                dsInventory.AcceptChanges();
+                Response.Redirect("Default.aspx");
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Text = "Failed to update";
+                lblMessage.Visible = true;
+            }
         }
 
         protected void cboHelp_CheckedChanged(object sender, EventArgs e)
         {
-             lblMeasuerHelp.Visible = lblProducthelp.Visible = lblQuantityuHelp.Visible = lblSizeHelp.Visible = lblPriceHelp.Visible = cboHelp.Checked;
+            lblMeasureHelp.Visible = lblProducthelp.Visible =
+                lblBrandHelp.Visible = lblDescHelp.Visible =
+                lblPriceHelp.Visible = lblQtyHelp.Visible =
+                lblSizeHelp.Visible = cboHelp.Checked;
         }
 
         protected void btnLogout_Click(object sender, EventArgs e)
